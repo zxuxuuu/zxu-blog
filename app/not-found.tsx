ac8e198f5d6878dea25459d2eb203759682a223d
zhangxu <1003146537@qq.com>
2025-05-27 16:34:07 +0800

    feat: 404页面汉化

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -9,15 +9,13 @@ export default function NotFound() {
         </h1>
       </div>
       <div className="max-w-md">
-        <p className="mb-4 text-xl leading-normal font-bold md:text-2xl">
-          Sorry we couldn't find this page.
-        </p>
-        <p className="mb-8">But dont worry, you can find plenty of other things on our homepage.</p>
+        <p className="mb-4 text-xl leading-normal font-bold md:text-2xl">访问的页面不存在</p>
+        <p className="mb-8">别担心，你可以在我的主页上找到很多其他的东西</p>
         <Link
           href="/"
           className="focus:shadow-outline-blue inline rounded-lg border border-transparent bg-blue-600 px-4 py-2 text-sm leading-5 font-medium text-white shadow-xs transition-colors duration-150 hover:bg-blue-700 focus:outline-hidden dark:hover:bg-blue-500"
         >
-          Back to homepage
+          返回首页
         </Link>
       </div>
     </div>
